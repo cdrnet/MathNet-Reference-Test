f6af2af4dd15257d86bd038f9379744f96e6e098
Christoph Ruegg <git@cdrnet.ch>
2019-06-01 14:31:31 +0200

    Project Skeletons

---
+++ Net472.CSharp.StrongName/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Net472.CSharp.StrongName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
