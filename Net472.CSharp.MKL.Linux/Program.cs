cbbd928a300258c1a2aa58162b7c91d1db3195d5
Christoph Ruegg <git@cdrnet.ch>
2019-06-01 15:31:38 +0200

    Separate projects for native providers

---
+++ Net472.CSharp.MKL.Linux/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Net472.CSharp.MKL.Linux
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MathNet.Numerics.Control.Describe());
+        }
+    }
+}
---
+++ Net472.CSharp.MKL.Win.Manual/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Net472.CSharp.MKL.Win.Manual
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MathNet.Numerics.Control.Describe());
+        }
+    }
+}
